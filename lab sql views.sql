01a35922c6ef82fb40e3a56fe900eca567685ace
alexferrer <alexferrer213@gmail.com>
2023-12-07 17:34:02 +0100

    Solved Lab v2

--- lab sql views.sql
+++ lab sql views.sql
@@ -8,7 +8,8 @@ n about customers in the Sakila database, including their rental history and pay
  -- step 1. First, create a view that summarizes rental information for each customer. 
  -- The view should include the customer's ID, name, email address, and total number of rentals (rental_count).
 use sakila;
-select * from rental_info;
+
+-- drop view if exists for safeguards
 DROP VIEW IF EXISTS rental_info; 
 
 CREATE VIEW rental_info as (
@@ -19,7 +20,7 @@ CREATE VIEW rental_info as (
 		count(rental_id) as total_number_of_rental
 	FROM
 		rental r
-	RIGHT JOIN customer c ON r.customer_id = c.customer_id
+	RIGHT JOIN customer c ON r.customer_id = c.customer_id -- right join to preserve every single customer ID.
 	GROUP BY r.customer_id, cust_name, c.email
 );
 
@@ -28,6 +29,10 @@ CREATE VIEW rental_info as (
 -- The Temporary Table should use the rental summary view created in Step 1 to join with the payment table 
 -- and calculate the total amount paid by each customer.
 */
+
+-- drop temp table if exists for safeguards
+DROP TEMPORARY TABLE IF EXISTS rental_pmt_info; 
+
 CREATE TEMPORARY TABLE rental_pmt_info as (
 	SELECT 
 		ro.customer_id,
@@ -37,7 +42,7 @@ CREATE TEMPORARY TABLE rental_pmt_info as (
 		sum(p.amount) as total_paid
 	FROM
 			payment p
-	RIGHT JOIN rental_info ro ON p.customer_id = ro.customer_id
+	RIGHT JOIN rental_info ro ON p.customer_id = ro.customer_id -- right join so that all customers are included
 
 	GROUP BY 
 			ro.customer_id,
@@ -45,7 +50,7 @@ CREATE TEMPORARY TABLE rental_pmt_info as (
 			ro.email, 
 			ro.total_number_of_rental
 );
-    
+
 /*Step 3: Create a CTE and the Customer Summary Report
 -- Create a CTE that joins the rental summary View with the customer payment summary Temporary Table
 -- created in Step 2. The CTE should include the customer's name, email address, rental count, and total amount paid.
@@ -59,9 +64,10 @@ WITH CTE_customer_rental_info  AS
 (
   SELECT 
 	*, 
-    round((total_paid/total_number_of_rental),2) as average_payment_per_rental 
+    round((total_paid/total_number_of_rental),2) as average_payment_per_rental  -- to obtain average payment per rental
 FROM 
 	rental_pmt_info
 )
-SELECT * FROM CTE_customer_rental_info
-WHERE customer_id = 1;
+SELECT * FROM CTE_customer_rental_info; -- select from CTE 
+-- WHERE customer_id = 1; 
+-- use the where statement as needed to show the selected customer 
